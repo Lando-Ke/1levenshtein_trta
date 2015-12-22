fb0de89601dd0dd0dd26aad876d145fc313506c6
Gabriel Lando <glando.py@gmail.com>
2015-12-22 09:50:57 +0300

    commits Levenshtein distance v1
    One Levenshtein to rule them all

---
+++ lynk.py
@@ -0,0 +1,61 @@
+#lynk itnerview question
+#
+#@author Gabriel Lando
+#
+#Given a wordlist (newline separated file of words) and given two words as input parameters, 
+#find the minimal path from the first word to the second in the word list where each step can 
+#be only of Levenshtein distance 1 (a change, insertion or deletion of a single letter).
+#
+#reference 1: https://en.wikipedia.org/wiki/Levenshtein_distance
+#reference 2: http://www.geeksforgeeks.org/dynamic-programming-set-5-edit-distance/
+
+# read dictionary file
+#words = open(DICTIONARY, "rt").read().split();
+
+def dist_copmute(word1, word2):
+	#Set the lengths and construct matrix T using m rows and n columns
+	##Fill using bottom up dp aproach
+	m = len(word1) + 1
+	n = len(word2) + 1
+	T = [[0 for x in xrange(n)] for x in xrange(m)]
+
+	for i in xrange(1, m):
+		T[0][i] = i
+    
+    #insterts, deletions and substitutions
+	for i in xrange(1, m):
+		for j in xrange(1, n):
+			if word1[i-j] == word2[j-1]:
+				T[i][j] = T[i-1][j-1]
+			else:
+				T[i][j] = 1 + min(T[i-1][j-1], T[i-1][j], T[i][j-1])
+
+	getEdited(T, word1, word2)
+	return T[m-1][n-1]
+
+def getEdited(T, word1, word2):
+	#prints what was edited
+	#contains provision for empty strings
+	i = len(T) - 1
+	j =len(T[0]) -1
+	while True:
+		if i == 0 or j == 0:
+			break
+		if word1[i-1] == word2[j-1]:
+			i = i-1
+			j = j-1
+		elif T[i][j] == T[i-1][j-1] + 1:
+			print "Edit %s in the second word to %s in the first word" %(word2[j-1], word1[i-1])
+			i = j-1
+			j = i-1
+		elif T[i][j] == T[i-1][j] + 1:
+			print "Delete %s from the first word" %(word1[i-1])
+			i = i-1
+		elif T[i][j] == T[i][j] + 1:
+			print "Delete %s in the second word" %(word2[j-1])
+			j = j-1
+
+
+if __name__ == '__main__':
+	answer = dist_copmute(list('Test'), list('Trist'))
+	print(answer)
